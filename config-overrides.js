07455ad0b666469c2ab5161a58ad2b97f8fff085
littleee <liubochao_job@126.com>
2020-06-22 02:10:26 +0800

    fix

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,10 @@
-const { override, fixBabelImports, addWebpackPlugin, useBabelRc, disableEsLint } = require('customize-cra');
-const {injectBabelPlugin} = require('react-app-rewired');
-const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
+const { override, fixBabelImports, useBabelRc, disableEsLint } = require('customize-cra');
 
  module.exports = override(
    fixBabelImports('antd', {
      libraryDirectory: 'es',
      style: 'css',
    }),
-   addWebpackPlugin(new AntdDayjsWebpackPlugin()),
    disableEsLint(),
    useBabelRc()
  );
